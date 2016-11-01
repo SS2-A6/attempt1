220c0f31330695884b677206a4f5cdb9766b464f
Yamashita <under_mt_d@yahoo.co.jp>
2016-11-01 10:57:40 +0900

    timer encoder

--- attempt1_test/attempt1_test.ino
+++ attempt1_test/attempt1_test.ino
@@ -1,13 +1,14 @@
 #include <stdio.h>
- 
+#include <MsTimer2.h>
+
 static FILE uartout;
- 
+
 static int uart_putchar (char c, FILE *stream) {
-    if (Serial.write(c) > 0) {
-      return 0;
-    } else {
-      return -1;
-    }
+  if (Serial.write(c) > 0) {
+    return 0;
+  } else {
+    return -1;
+  }
 }
 
 #define LED 2
@@ -16,7 +17,8 @@ unsigned int low = 0;
 unsigned int thres = 630;
 byte flag = 0;
 unsigned int count = 0;
-word val = 0;
+word val1 = 0;
+word val2 = 0;
 byte before = 0;
 byte pri = 0;
 
@@ -42,26 +44,39 @@ void setup() {
   analogWrite(5, 255);
   analogWrite(6, 0);
 
+  // タイマ割り込み設定
+  MsTimer2::set(50, encoder);
+  MsTimer2::start();
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  val =analogRead(0);
-  //printf("%d\n", val);
-  if ( val >= high) {
-    if ( flag == 1 ){
-        printf("V 0 %d\n", val);
-        //count++;
-    }    
+
+// エンコーダ読み取り関数 (タイマ割り込み)
+void encoder() {
+
+  val1 = analogRead(0);
+  val2 = analogRead(1);
+  //printf("%d %d\n", val1, val2);
+  if ( val1 >= high) {
+    if ( flag == 1 ) {
+      printf("V 0 %d\n", val1);
+      //count++;
+    }
     flag = 0;
   }
-  else if ( val <= low ) {
-    if (flag == 0){
-        printf("^ 1 %d %d\n", val, count);
-        count++;
+  else if ( val1 <= low ) {
+    if (flag == 0) {
+      printf("^ 1 %d %d\n", val1, count);
+      count++;
     }
     flag = 1;
   }
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
 
   if (count % 32 == 0) {
     if (pri == 0) {
@@ -72,20 +87,21 @@ void loop() {
       delay(1000);
       analogWrite(5, 255);
       analogWrite(6, 0);
-    } 
+    }
   } else {
     pri = 0;
   }
+  
 }
 
 #else
-void setup(){
+void setup() {
   Serial.begin(9600);
   fdev_setup_stream (&uartout, uart_putchar, NULL, _FDEV_SETUP_WRITE);
   stdout = &uartout;
 }
-void loop(){
-  val =analogRead(0);
+void loop() {
+  val = analogRead(0);
   printf("%d\n", val);
 }
 #endif
